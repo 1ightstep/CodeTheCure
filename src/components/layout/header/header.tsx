a8a43c86ab74b524780691819e4ee13ca32ec5f6
lightsteps <reallightsteps@gmail.com>
2025-07-08 18:27:55 -0700

    header layout finished

---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import styles from "./header.module.css";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerContainer}>
+        <div className={styles.navBarContainer}>
+          <div className={styles.logoContainer}>
+            <Link href="/" className={styles.logo}>
+              <Image
+                src="/code-the-cure.png"
+                alt="Code The Cure Logo"
+                width={100}
+                height={100}
+              />
+            </Link>
+          </div>
+          <nav className={styles.navBar}>
+            <ul className={styles.navLinks}>
+              <li>
+                <Link href="/contact">About</Link>
+              </li>
+              <li>
+                <Link href="/about">FAQ</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.authButtons}>
+          <button className={styles.loginButton}>Login</button>
+          <button className={styles.signupButton}>Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
+}
